d048a655937429f947ba1f5e1352f2cea3363dd1
KelvinyHenrique <kelvinyhenrique17@gmail.com>
2017-08-24 18:29:48 -0300

    Ajustei o HTML

--- portfolios/views.py
+++ portfolios/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import projeto
+
 
 def portfolios_exibir(request):
-	return render(request, 'projetos/index.html', {})
+	
+	meuprojeto = projeto.objects.all()
+	context = {'meuprojeto': meuprojeto} 
+	return render(request, 'projetos/index.html', context)
+
 
